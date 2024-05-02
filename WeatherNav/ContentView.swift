c8c903a74f096d659de001474df4b7c38d3fe7bb
Sofia Leong <sofia.leong@tui.com>
2024-05-02 10:51:15 +0100

    Initial Commit

---
+++ WeatherNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherNav
+//
+//  Created by SofiaLeong on 02/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherNav/WeatherNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNavApp.swift
+//  WeatherNav
+//
+//  Created by SofiaLeong on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
